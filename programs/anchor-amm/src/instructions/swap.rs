84927800760ea4a1032260124478e52e50d6ac8e
Denimsun Basumatary <denimbasumatary@outlook.com>
2025-07-28 14:30:20 +0530

    add swap instruction

--- programs/anchor-amm/src/instructions/swap.rs
+++ programs/anchor-amm/src/instructions/swap.rs
@@ -1,10 +1,11 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{Mint, Token, TokenAccount},
+    token::{transfer, Mint, Token, TokenAccount, Transfer},
 };
+use constant_product_curve::{ConstantProduct, LiquidityPair};
 
-use crate::Config;
+use crate::{error::AmmError, Config};
 
 #[derive(Accounts)]
 pub struct Swap<'info> {
@@ -14,9 +15,19 @@ pub struct Swap<'info> {
     pub mint_y: Account<'info, Mint>,
     #[account(
         seeds = [b"lp", config.key().as_ref()],
-        bump = config.lp_bump
+        bump = config.config_bump
     )]
     pub mint_lp: Account<'info, Mint>,
+    #[account(
+        associated_token::mint = mint_x,
+        associated_token::authority = user
+    )]
+    pub user_x: Account<'info, TokenAccount>,
+    #[account(
+        associated_token::mint = mint_y,
+        associated_token::authority = user
+    )]
+    pub user_y: Account<'info, TokenAccount>,
     #[account(
         associated_token::mint = mint_x,
         associated_token::authority = config
@@ -36,4 +47,94 @@ pub struct Swap<'info> {
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
+}
+
+impl<'info> Swap<'info> {
+    fn swap(&mut self, is_x: bool, amount: u64, min: u64) -> Result<()> {
+        require!(self.config.locked != true, AmmError::PoolLocked);
+        require!(amount != 0, AmmError::InvalidAmount);
+
+        let mut curve = ConstantProduct::init(
+            self.vault_x.amount,
+            self.vault_y.amount,
+            self.mint_lp.supply,
+            self.config.fee,
+            None,
+        )
+        .unwrap();
+
+        let pair = match is_x {
+            true => LiquidityPair::X,
+            false => LiquidityPair::Y,
+        };
+
+        let res = curve.swap(pair, amount, min).unwrap();
+
+        require!(res.deposit != 0, AmmError::InvalidAmount);
+        require!(res.withdraw != 0, AmmError::InvalidAmount);
+
+        self.deposit_tokens(is_x, res.deposit)?;
+        self.withdraw_tokens(is_x, res.withdraw)?;
+
+        Ok(())
+    }
+
+    fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = match is_x {
+            true => (
+                self.user_x.to_account_info(),
+                self.vault_x.to_account_info(),
+            ),
+            false => (
+                self.user_y.to_account_info(),
+                self.vault_y.to_account_info(),
+            ),
+        };
+
+        let accounts = Transfer {
+            from,
+            to,
+            authority: self.user.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(self.token_program.to_account_info(), accounts);
+        transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+
+    fn withdraw_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let seeds = &[
+            &b"config"[..],
+            &self.config.seed.to_le_bytes(),
+            &[self.config.config_bump],
+        ];
+        let signer = &[&seeds[..]];
+        let (from, to) = match is_x {
+            true => (
+                self.vault_x.to_account_info(),
+                self.user_x.to_account_info(),
+            ),
+            false => (
+                self.vault_y.to_account_info(),
+                self.user_y.to_account_info(),
+            ),
+        };
+
+        let accounts = Transfer {
+            from,
+            to,
+            authority: self.config.to_account_info(),
+        };
+
+        let cpi_ctx =
+            CpiContext::new_with_signer(self.token_program.to_account_info(), accounts, signer);
+        transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+}
+
+pub fn handler(ctx: Context<Swap>, is_x: bool, amount: u64, min: u64) -> Result<()> {
+    ctx.accounts.swap(is_x, amount, min)
 }
